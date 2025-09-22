978fa6389f5f91e36029a66fc81d0c86afee0956
Pasek108 <pasartur108@gmail.com>
2025-09-22 19:52:43 +0200

    heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_13_170516) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
@@ -25,8 +28,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_13_170516) do
   end
 
   create_table "user_events", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "event_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
     t.integer "role", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
